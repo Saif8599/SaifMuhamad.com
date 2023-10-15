0e3c1c7976f7f4768a858de8fb5e7cb0adc0bc57
Saif <ninja_saif@hotmail.com>
2023-10-15 14:40:44 +0200

    Change Navbar styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cea9390803fcda9"; }
+/******/ 		__webpack_require__.h = function() { return "b3319f9c951c07ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/features/blocks/navbar/NavbarBlock.tsx
+++ src/features/blocks/navbar/NavbarBlock.tsx
@@ -29,7 +29,7 @@ const NavbarBlock: React.FC<NavbarProps> = () => {
   return (
     <nav
       className={`flex items-center justify-between flex-wrap py-4 md:px-12 bg-transparent ${
-        isScrolled ? 'fixed top-0 w-full z-50 backdrop-blur-lg bg-opacity-90' : ''
+        isScrolled ? 'fixed top-0 w-full z-50 bg-opacity-90' : ''
       }`}
     >
       <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 lg:pb-0">
@@ -76,32 +76,9 @@ const NavbarBlock: React.FC<NavbarProps> = () => {
             Contact
           </a>
         </div>
-        <div className="relative mr-2 text-gray-600 lg:block hidden font-mono">
-          <input
-            className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
-            type="search"
-            name="search"
-            placeholder="Search"
-          />
-          <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">
-            <svg
-              className="text-gray-600 h-4 w-4 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              id="Capa_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 56.966 56.966"
-              width="512px"
-              height="512px"
-            >
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-            </svg>
-          </button>
-        </div>
         <div className="flex font-mono">
           <a
-            href="#"
+            href="YouCantGetInHaHa"
             className="block text-md px-4 py-1 rounded text-red-700 font-bold hover:text-white mt-4 lg:mt-0 border-red-500 transition-all duration-500"
           >
             Login
